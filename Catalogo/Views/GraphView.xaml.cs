617bce508cdca4880e5a89a5ba1c343f967b920f
Irene Dueñas Rojas <irene.duennas@alumno.iesluisvives.org>
2025-02-06 10:14:43 +0100

    Fin

--- Catalogo/Views/GraphView.xaml.cs
+++ Catalogo/Views/GraphView.xaml.cs
@@ -8,6 +8,11 @@ namespace Catalogo.Views;
 
 public partial class GraphView : UserControl
 {
+    public GraphView()
+    {
+        InitializeComponent();
+    }
+
     public GraphView(IServiceRepository<Category> categoryService, IServiceRepository<Product> productService)
     {
         InitializeComponent();
